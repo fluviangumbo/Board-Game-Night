0c20e211be7459ee0d1f63139e242262b1827933
joel <jroney002@gmail.com>
2024-09-29 18:30:00 -0500

    added access model, need to refactor and use through tables in sequelize, redundnacies need to be eliminated

--- server/src/models/index.ts
+++ server/src/models/index.ts
@@ -2,10 +2,12 @@ import sequelize from '../config/connection.js';
 import { UserFactory } from './user.js';
 import { GroupFactory } from './group.js';
 import { AccessFactory } from './access.js';
+import { GameFactory } from './game.js';
 // ALL OTHER MODELS IMPORT HERE IF WE MAKE THEM
 
 const User = UserFactory(sequelize);
 const Group = GroupFactory(sequelize);
 const Access = AccessFactory(sequelize);
+const Game = GameFactory(sequelize);
 
-export { User, Group, Access };
+export { User, Group, Access, Game };
