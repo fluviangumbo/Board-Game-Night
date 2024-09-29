c79735d406fc13b9628f3c34a556aa4d881923f6
joel <jroney002@gmail.com>
2024-09-29 17:49:50 -0500

    added group and access models and framework for controllers

---
+++ server/src/controllers/accessController.ts
@@ -0,0 +1,12 @@
+import type { Request, Response } from 'express';
+import { Access } from '../models/index.js';
+
+// TODO: Code out the functions for access controller
+export const getGroup = async () => {
+    return
+};
+
+export const getMember = async () => {
+    return
+};
+
--- server/src/models/index.ts
+++ server/src/models/index.ts
@@ -1,7 +1,11 @@
 import sequelize from '../config/connection.js';
 import { UserFactory } from './user.js';
+import { GroupFactory } from './group.js';
+import { AccessFactory } from './access.js';
 // ALL OTHER MODELS IMPORT HERE IF WE MAKE THEM
 
 const User = UserFactory(sequelize);
+const Group = GroupFactory(sequelize);
+const Access = AccessFactory(sequelize);
 
-export { User };
+export { User, Group, Access };
