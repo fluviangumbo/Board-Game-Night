c6ba3fea246f5f76dfcb0d19ebfe80605a12f6dd
Laura Schooley <laura.schooley3@gmail.com>
2024-10-01 22:07:20 -0500

    ???

--- server/src/models/access.ts
+++ server/src/models/access.ts
@@ -1,16 +1,12 @@
-
 import { Model, type InferAttributes, type InferCreationAttributes, type CreationOptional, DataTypes, type Sequelize, type ForeignKey, } from 'sequelize';
 import type { User } from './user.js';
 import type { Group } from './group.js';
-
-
 export class Access extends Model<InferAttributes<Access>, InferCreationAttributes<Access>> {
     declare accessId: CreationOptional<number>;
     declare user: ForeignKey<User['id']>;
     declare group: ForeignKey<Group['id']>;
     declare level: string;
 }
-
 export function AccessFactory(sequelize: Sequelize): typeof Access {
     Access.init(
         {
@@ -30,38 +26,5 @@ export function AccessFactory(sequelize: Sequelize): typeof Access {
             hooks: {},
         }
     );
-
     return Access;
 }
-
-import { Model, type InferAttributes, type InferCreationAttributes, type CreationOptional, DataTypes, type Sequelize, type ForeignKey, } from 'sequelize';
-import type { User } from './user.js';
-import type { Group } from './group.js';
-
-export class Access extends Model<InferAttributes<Access>, InferCreationAttributes<Access>> {
-    declare accessId: CreationOptional<number>;
-    declare user: ForeignKey<User['id']>;
-    declare group: ForeignKey<Group['id']>;
-    declare level: string;
-}
-
-export function AccessFactory(sequelize: Sequelize): typeof Access {
-    Access.init(
-        {
-            accessId: {
-                type: DataTypes.INTEGER,
-                primaryKey: true,
-                autoIncrement: true,
-            },
-            level: {
-                type: DataTypes.ENUM('Owner', 'Admin', 'Member'),
-                allowNull: false,
-            },
-        },
-        {
-            tableName: 'access',
-            sequelize,
-            hooks: {},
-        }
-    );
-};
