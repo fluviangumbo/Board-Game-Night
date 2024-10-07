327e3fabb77dc4073f030e71cafd8f6ccd128ff1
joel <jroney002@gmail.com>
2024-10-06 20:06:39 -0500

    fixed game seeding

---
+++ server/src/seeds/game-seeds.ts
@@ -0,0 +1,39 @@
+import { Game } from '../models/index.js';
+// import { type Game as GameType } from '../models/game.js';
+import csvParser from 'csv-parser';
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+interface GameType {
+    id: number;
+    name: string;
+    published: number;
+    rank: number;
+}
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export const seedGames = async () => {
+    const games: GameType[] = [];
+
+    fs.createReadStream(path.join(__dirname, '../../db/boardgames_ranks.csv'))
+      .pipe(csvParser())
+      .on('data', (row: any) => {
+        games.push({
+            id: row.id,
+            name: row.name,
+            published: row.yearpublished,
+            rank: row.rank,
+        });
+      })
+      .on('end', async () => {
+        try {
+            await Game.bulkCreate(games);
+            console.log('Games table seeded successfully.\n\n');
+        } catch (err) {
+            console.error('Error seeding games table.', err);
+        }
+      });
+}
