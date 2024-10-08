af002da848c6ebdd4ea4730e6d7666f80ca2abc4
Laura Schooley <laura.schooley3@gmail.com>
2024-10-08 17:18:18 -0500

    header fixed

--- client/src/components/header.tsx
+++ client/src/components/header.tsx
@@ -2,8 +2,8 @@
 const Header = () => {
 return (
     <div className="header">
-        <h1>Connect4Fun</h1>
-        <p>Where there's a board, there's a way</p>
+        <h1 className="name">Connect4Fun</h1>
+        <p className="tagline d-block">Where there's a board, there's a way</p>
     </div>
     );
 }
