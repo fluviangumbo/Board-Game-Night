7920ccbad8f1da0c563ccebb2550648261815708
Laura Schooley <laura.schooley3@gmail.com>
2024-10-13 14:25:56 -0500

    Styled Navbar but having issues now with link to groups

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,6 +1,7 @@
-import { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import auth from '../utils/auth';
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import auth from "../utils/auth";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const Navbar = () => {
   const [loginCheck, setLoginCheck] = useState(false);
@@ -17,34 +18,71 @@ const Navbar = () => {
   }, [loginCheck]);
 
   return (
-    <div className='display-flex justify-space-between align-center py-2 px-5 mint-green login-button'>
-      <div style={{ display: 'flex', gap: '15px' }}>
-        {!loginCheck ? (
-          <button className='btn' type='button'>
-            <Link to='/login'>Login</Link>
-          </button>
-        ) : (
-          <button
-            className='btn'
-            type='button'
-            onClick={() => {
-              auth.logout();
-            }}
+    <div className="display-flex justify-space-between align-center">
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark align-center">
+        <div className="container-fluid">
+          <a
+            className="navbar-brand"
+            style={{ display: "flex", gap: "30px", paddingLeft: "15px" }}
           >
-            Logout
+            Connect4Fun
+          </a>{" "}
+          <span className="navbar-text">
+            where there’s a board there’s a way
+          </span>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarText"
+            aria-controls="navbarText"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
           </button>
-        )}
-        <button className='btn' type='button'>
-          <Link to='/hot-games'>HotGames</Link>
-        </button>
-
-        <button className='btn' type='button'>
-          <Link to='/groups'>Groups</Link>
-        </button>
-        <button className='btn' type='button'>
-          <Link to='/calendar'>Create an Event</Link>
-        </button>
-      </div>
+          <div className="collapse navbar-collapse navbar-align-right" id="navbarText">
+            <ul className="navbar-nav ms-auto">
+              <li className="nav-item">
+                <div style={{ display: "flex", gap: "15px" }}>
+                  {!loginCheck ? (
+                    <button className="btn" type="button">
+                      <Link to="/login">Login</Link>
+                    </button>
+                  ) : (
+                    <button
+                      className="btn"
+                      type="button"
+                      onClick={() => {
+                        auth.logout();
+                      }}
+                    >
+                      Logout
+                    </button>
+                  )}
+                </div>
+              </li>
+              <li className="nav-item">
+                <div>
+                  <button className="btn" type="button">
+                    <Link to="/hot-games">Hot Games</Link>
+                  </button>
+                </div>
+              </li>
+              <li className="nav-item">
+                <button className="btn" type="button">
+                  <Link to="/groups">Groups</Link>
+                </button>
+              </li>
+              <li className="nav-item">
+                <button className="btn" type="button">
+                  <Link to="/calendar">Create an Event</Link>
+                </button>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
     </div>
   );
 };
